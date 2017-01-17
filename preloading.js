3928c91a735074ea727fde09b671a6f6099af654
Tom <tomnik104@yahoo.de>
2017-01-17 21:23:28 +0100

    Some animations and shiat

--- preloading.js
+++ preloading.js
@@ -1,8 +1,20 @@
-$( document ).ready(function() {
-  console.log( "ready!" );
-    	$('#preloading').fadeOut('slow',function(){
-        $('#afterloading').fadeIn('slow',function(){
-          console.log( "ready!" );
-    });
-  });
+$(document).ready(function () {
+    console.log("ready!");
+
+    $('#isLoading').addClass('blur');
+    $('#isLoading').fadeOut('slow', function () {
+        console.log('Fading');
+        $({blurRadius: 5}).animate({blurRadius: 0}, {
+            duration: 500,
+            easing: 'swing',
+            step: function () {
+                $('#blur').css({
+                    "-webkit-filter": "blur(" + this.blurRadius + "px)",
+                    "filter": "blur(" + this.blurRadius + "px)"
+                });
+            }
+        });
+        console.log('Loading ready');
+
+    })
 });
