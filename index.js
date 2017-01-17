b83aeb44bfa79107ecfcbc3ac75f84be0ac3c6ef
Tom <tomnik104@yahoo.de>
2017-01-17 20:15:18 +0100

    blur

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 /*
  * Created by tom on 17/01/2017.
  */
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
+
+    $('#loading').addClass('blur');
     $('#loading').fadeOut('slow', function () {
         $('#afterLoading').fadeIn('slow', function () {
-           console.log('Loading ready');
+            console.log('Loading ready');
+
         });
     })
 });
