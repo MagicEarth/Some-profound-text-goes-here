f0eaccb5c6fc79ebe3cc0a27d8ffc000b3228cc1
MagicEarth <robin.bernsee@online.de>
2017-01-17 20:54:14 +0100

    Create homepage..php

---
+++ homepage..php
@@ -0,0 +1,39 @@
+<html>
+<head>
+  <title>Homepage</title>
+  <link rel="stylesheet"  type="text/css" href="css/preloading.css">
+  <link rel="stylesheet"  type="text/css" href="css/homepage.css">
+  <link rel="icon" type="image/favicon.png" href="pictures/favicon/favicon.png" sizes="32x32">
+  <meta charset="utf-8">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="js/preloading.js"></script>
+</head>
+<body>
+<div id="preloading">
+<div id="afterloading">
+  Spielvorbereitung
+Vor dem Beginn einer Runde kann ein Spieler mit Hilfe eines automatischen Matchmaking-Systems Mitspieler und Gegner suchen. Alternativ kann ein Spieler für ein freies Spiel seine Mitspieler manuell suchen oder aus seiner Freundesliste einladen. In diesem Fall findet das Matchmaking-System keine Verwendung. Sind genügend Spieler für beide Teams gefunden (fünf je Team im klassischen Modus), landen alle Spieler der Partie gemeinsam in der Champion-Auswahl. Hier kann jeder Spieler seinen gewünschten Champion, eine Spielfigur mit individuellen Fähigkeiten, Stärken und Schwächen, auswählen. Der Spieler erfüllt dabei die Rolle des Beschwörers, der diese Figur beschwört und steuert. Je nach Spielmodus können alle Spieler gleichzeitig wählen oder jeweils abwechselnd pro Team. Im abwechselnden Auswahlmodus können zudem vor der Auswahl von drei unterschiedlichen Mitgliedern jedes Teams abwechselnd je ein Champion „gebannt“ werden. Gebannte Champions können von keinem Spieler beider Teams gewählt werden.
+
+Neben dem Champion können zwei Beschwörerzauber gewählt werden, welche im Spiel verwendet werden können. Diese sind vom gewählten Champion unabhängig. Zudem kann aus diversen Fertigkeitsverbesserungen, den Runen und Meisterschaften, ausgewählt werden, um den Champion an die individuelle Spielstrategie anzupassen.
+
+Spielverlauf
+Das Spielfeld selbst ist in zwei ähnlich gehaltene Hälften aufgeteilt, von denen jede einem Team zugeordnet ist. Je nach Karte und Spielmodus gibt es mehrere Wege zwischen beiden Kartenhälften. Die Wege werden im Spieljargon allgemein als Lanes (englisch lane, dt.: „Fahrspur“ oder „Weg“) bezeichnet, wobei das Gebiet zwischen diesen häufig als „Dschungel“ zusammengefasst wird. In diesem befinden sich diverse neutrale (keinem Team zugehörige) Monster, d. h., Mitglieder beider Teams können sie besiegen. Einige dieser Monster verleihen einem Champion – zusätzlich zu Erfahrung und Gold – einzigartige, zeitlich eingeschränkte Verbesserungen. Das Spielfeld ist nur an denjenigen Stellen aufgedeckt, wo sich gerade ein Spieler, ein Turm, Vasallen oder ein sogenannter "Ward" (englisch ward „Auge“), ein einmalig verwendbarer Gegenstand, der für eine begrenzte Zeitspanne einen kleinen Sichtbereich erschafft, des eigenen Teams befinden. Der Rest ist im sogenannten Kriegsnebel verborgen, also für Spieler nicht einsehbar.
+
+Je zwei Teams aus in der Regel drei oder fünf Spielern treten gegeneinander an. Die beiden Teams starten auf gegenüberliegenden Seiten einer Karte in der Nähe eines Hauptgebäudes, das Nexus genannt wird. Ziel ist es, den gegnerischen Nexus zu zerstören. Der Nexus wird dabei vom gegnerischen Team sowie von computergesteuerten Einheiten (sogenannten „Vasallen“, im engl. Minions) und Türmen verteidigt. Die Vasallen werden in die Richtung des gegnerischen Hauptgebäudes entsandt und folgen den jeweiligen „Lanes“, sie laufen nicht durch den „Dschungel“. Außerdem greifen sie nahe Gegner an. Außer im Spielmodus „Dominion“ gibt es neben dem Nexus noch Inhibitoren. Falls sie zerstört werden, werden auf der Gegenseite der entsprechenden „Lane“ stärkere Vasallen erscheinen – sogenannte Supervasallen. Um einen zu schnellen Spielfortschritt zu verhindern, kann der Nexus nicht direkt angegriffen werden. Es müssen zuerst auf mindestens einer „Lane“ alle gegnerischen Türme, also der äußere, innere und der Inhibitor-Turm, sowie der Inhibitor selbst zerstört werden. Anschließend werden die beiden Nexustürme, die unabhängig von einer „Lane“ sind, angreifbar, und erst nach deren Zerstörung kann auch der Nexus angegriffen werden.
+
+Durch das Töten von Vasallen, Monstern oder gegnerischen Champions gewinnen die Champions an Erfahrung, d. h., sie erreichen eine höhere Stufe („Level“), in der man neue Fähigkeiten freischalten oder diese verbessern kann, wobei diese durch den jeweiligen Champion bestimmt und nicht völlig frei wählbar sind. Zudem werden diverse grundlegende Attribute des jeweiligen Champions verbessert. Ferner gibt es für einen Spieler, der den endgültigen, tödlichen Schlag auf eine feindliche Einheit ausführt, einen gewissen Betrag an Gold. Dieses kann genutzt werden, um in der eigenen Basis spezielle Gegenstände für den Champion zu kaufen, die ihn stärker machen oder sich anderweitig positiv auswirken.
+
+Ein Spiel dauert je nach Spielmodus, Karte und Spieleranzahl im Regelfall etwa 15 bis 60 Minuten, wobei es jedoch möglich ist, eine Partie vorzeitig durch Kapitulation zu beenden. Dieser müssen mindestens 70 % der Spieler des betroffenen Teams zustimmen, und auf den Karten Kluft der Beschwörer und Heulende Schlucht müssen mindestens 20 Minuten beziehungsweise auf den Karten Gewundener Wald und Kristallnarbe 15 Minuten vergangen sein.[4]
+
+Dauerhafte Spielmodi
+Es gibt insgesamt drei Karten mit unterschiedlichen Spielmodi. Darunter ist die klassische 5vs5-Karte (Kluft der Beschwörer), eine 3vs3-Karte (Der Gewundene Wald) und eine Karte für ARAM (Heulende Schlucht). Dazu gibt es verschiedene Spielmodi: Ranglistenspiele, normale Spiele, freie Spiele und kooperative Spiele. Außerdem gibt es einen Beobachtermodus und ein Tutorial. Abgeschlossene Spiele werden in der persönlichen Spielhistorie angezeigt. Daneben gibt es gelegentlich temporär verfügbare Spielmodi.
+
+Ranglistenspiele werden in einem zentralen Ranglistensystem aufgezeichnet. Siege im Ranglistenspiel lassen den Spieler in einer Rangliste aufsteigen, Niederlagen absteigen. Ranglistenspiele sind wie normale Spiele, allerdings setzen diese ein Beschwörerlevel von 30 voraus sowie eine Mindestanzahl von 16 Champions, um alleine oder in einer Gruppe zu zweit oder zu fünft (beziehungsweise zu dritt im Gewundenen Wald) teilnehmen zu können.
+In Kooperative Spielen kann der Spieler entweder allein oder als Teil einer Gruppe gegen ein Team von Nicht-Spieler-Charakteren antreten.
+Bei freien Spielen können die Spieler manuell individuelle Spielesitzungen erstellen, denen andere Spieler auf einer Spielliste beitreten können.
+Kluft der Beschwörer
+Kluft der Beschwörer (engl. Summoner's Rift) ist die meistgespielte Karte, auf ihr wird „5 gegen 5“ gespielt. Die Karte besitzt drei Lanes mit jeweils drei Türmen pro Team und einem Inhibitor pro Lane. Üblicherweise werden zwei der Lanes („top“ und „mid“, also obere und mittlere Lane) von jeweils einem Spieler verteidigt und die letzte Lane („bot“, von englisch bottom lane , dt: „unterer Weg“) von zwei Spielern. Der fünfte Spieler, der „Jungler“, hält sich im Dschungel zwischen den Lanes auf. Ein Vorteil davon ist, dass sich nicht zwei Champions die zu sammelnden Erfahrungspunkte teilen müssen. Zudem kann der Jungler „ganken“. Unter einem Gank versteht man das Überraschen eines Gegners mit einer plötzlichen Überzahlsituation, die zu einer Tötung (engl. "Kill") und somit zu Gold und Erfahrungspunkten führen soll.
+</div>
+</div>
+</body>
+</html>
